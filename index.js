1e6364949c8441cf78954562f43e5d0df5289320
cheesexuanyang <cheexuanyang@gmail.com>
2025-08-21 19:40:41 +0800

    Updated fixes

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://xuanyang-notes.netlify.app/login'
-  // Add your Netlify URL here after frontend deployment
+  'https://xuanyang-notes.netlify.app'
 ];
 
 app.use(cors({
